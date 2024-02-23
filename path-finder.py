f648b37fc1874b002ebd2a3bdb0ca894040603f0
alexandra-burca <alexandra.h.burca@gmail.com>
2024-02-23 13:35:31 +0200

    first commit

---
+++ path-finder.py
@@ -0,0 +1,36 @@
+import curses
+from curses import wrapper
+import queue
+import time
+
+maze = [
+    ["#", "#", "#", "#", "#", "O", "#", "#", "#"],
+    ["#", " ", " ", " ", " ", " ", " ", " ", "#"],
+    ["#", " ", "#", "#", " ", "#", "#", " ", "#"],
+    ["#", " ", "#", " ", " ", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", "#", "#"],
+    ["#", " ", " ", " ", " ", " ", " ", " ", "#"],
+    ["#", "#", "#", "#", "#", "#", "#", "X", "#"]
+]
+
+def print_maze(maze, stdscr, path=[]):
+    BLUE = curses.color_pair(1)
+    RED = curses.color_pair(2)
+
+    for i, row in enumerate(maze):
+        for j, value in enumerate(row):
+            stdscr.addstr(i, j*2, value, BLUE)
+
+def  main(stdsrc):
+    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    
+
+    stdsrc.clear()
+    print_maze(maze, stdsrc)
+    stdsrc.refresh()
+    stdsrc.getch()
+
+wrapper(main)
